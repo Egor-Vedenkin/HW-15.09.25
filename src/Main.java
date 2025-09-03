3a002c567b5b165b6db9da421e6d9e906c596708
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-09-04 02:44:16 +0300

    1'st task done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Вы достигли совершеннолетия");
+        } else {
+            System.out.println("Вы еще не достигли совершеннолетия, нужно подождать");
+        }
+        // Отступ для визуального отделения задач
+
+
 
     }
 }
