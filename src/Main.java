33078154306bbf9df35cdfb01dcf31ce7500fc63
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-09-04 02:49:19 +0300

    2'st task done

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
+        // Задача 1
         int age = 17;
         if (age >= 18) {
-            System.out.println("Вы достигли совершеннолетия");
+            System.out.println("Вы достигли совершеннолетия, вам " + age + " лет");
         } else {
-            System.out.println("Вы еще не достигли совершеннолетия, нужно подождать");
+            System.out.println("Вы еще не достигли совершеннолетия, нужно подождать, вам " + age + " лет");
         }
         // Отступ для визуального отделения задач
+        System.out.println();
 
+        // Задача 2
+        int temperature = 6;
+        if (temperature <= 6) {
+            System.out.println("Нужно надеть шапку, на улице " + temperature + " градусов");
+        } else {
+            System.out.println("Шапка не нужна, на улице " + temperature + " градусов");
+        }
+        // Отступ для визуального отделения задач
+        System.out.println();
 
+        // Задача 3
 
     }
 }
